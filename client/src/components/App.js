105c65331d170ac45c424cee4704101d2de53a2d
ThomasMartinUK <tmuk@protonmail.com>
2019-05-09 21:23:03 +0100

    Added components that will be used within the app; wired up components to the React-Router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
-const PageOne = () => {
-    return <div>PageOne</div>
-};
-
-const PageTwo = () => {
-    return (
-        <div>
-            PageTwo
-            <button>Click Me!</button>
-        </div>
-    );
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 const App = () => {
     return (
@@ -21,8 +13,11 @@ const App = () => {
                 <div>
                     {/* React Router cares only after the part of the url following the port/site definition */}
                     {/* Chooses to hide/show the component that matches the path definition */}
-                    <Route path = "/" exact component={PageOne}/>
-                    <Route path = "/pagetwo" component={PageTwo}/>
+                    <Route path="/" exact component = {StreamList} />
+                    <Route path="/streams/new" exact component = {StreamCreate} />
+                    <Route path="/streams/edit" exact component = {StreamEdit} />
+                    <Route path="/streams/delete" exact component = {StreamDelete} />
+                    <Route path="/streams/show" exact component = {StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
