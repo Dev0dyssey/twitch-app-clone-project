03703edf1977973038c2cd060e031379c5e13b7a
ThomasMartinUK <tmuk@protonmail.com>
2019-05-13 20:54:54 +0100

    Added the <Header />  component and placed it within the <BrowserRouter> as it contains  { Link }. Linked to paths, both to "/" as default. Adding more links to the <Header /> in the following commit to the App

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,16 +5,18 @@ import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './header';
 
 const App = () => {
     return (
-        <div>
+        <div className="ui container">
             {/* Any code placed outside of the <BrowserHeader /> will always be visible irrespective of the route */}
-            <h1>Header!</h1>
             <BrowserRouter>
                 <div>
                     {/* React Router cares only after the part of the url following the port/site definition */}
                     {/* Chooses to hide/show the component that matches the path definition */}
+                    {/* Any component containing a <Link></Link> tag must be placed within the Browser Router element */}
+                    <Header />
                     <Route path="/" exact component = {StreamList} />
                     <Route path="/streams/new" exact component = {StreamCreate} />
                     <Route path="/streams/edit" exact component = {StreamEdit} />
