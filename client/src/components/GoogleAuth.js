e38dcc24898fe1879f52f3772845bafbb9a53f47
ThomasMartinUK <tmuk@protonmail.com>
2019-06-01 08:05:04 +0100

    Update GoogleAuth.js

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -29,12 +29,12 @@ class GoogleAuth extends React.Component {
     };
 
     // Helper methods to handle Sign In and Sign Out when button is clicked
-    onSignIn = () => {
+    onSignInClick = () => {
         // this.auth = auth comes from the gapi call within componentDidMount
         this.auth.signIn();
     };
 
-    onSignOut = () => {
+    onSignOutClick = () => {
         this.auth.signOut();
     };
 
@@ -43,14 +43,14 @@ class GoogleAuth extends React.Component {
             return null;
         } else if(this.state.isSignedIn) {
             return (
-                    <button onClick = {this.onSignOut} className = "ui red google button">
+                    <button onClick = {this.onSignOutClick} className = "ui red google button">
                         <i className = "google icon" />
                         Sign Out
                     </button>
                 )
         } else {
             return (
-                    <button onClick = {this.onSignIn} className = "ui red google button">
+                    <button onClick = {this.onSignInClick} className = "ui red google button">
                         <i className = "google icon" />
                         Sign In with Google
                     </button>
