00d3396a31bf7a5f39b6142e0323e53c2a235e1d
ThomasMartinUK <tmuk@protonmail.com>
2019-06-02 07:50:09 +0100

    Moved type definition into a separate folder to make debugging easier; added userId capture to the gapi call and assigned userId as a new property within the Redux store for later use

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -27,7 +27,7 @@ class GoogleAuth extends React.Component {
     onAuthChange = (isSignedIn) => {
         // If statement to determine whether the use is signed in or not. Retrieved from the gapi
         if (isSignedIn) {
-            this.props.signIn();
+            this.props.signIn(this.auth.currentUser.get().getId());
         } else {
             this.props.signOut();
         }
@@ -70,6 +70,7 @@ class GoogleAuth extends React.Component {
 }
 
 const mapStateToProps = (state) => {
+    // .auth. taken from the index.js where we assigned the authReducer to the key of auth within the combineReducers({})
     return { isSignedIn: state.auth.isSignedIn };
 };
 
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,16 +1,20 @@
+// Define types in a separate file to help with debugging if the case is misspelled 
+import { SIGN_IN, SIGN_OUT } from '../actions/types';
+
 // Initial state (default state) = Initilazer;  to be passed into the reducer
 const INITIAL_STATE = {
-    isSignedIn: null
+    isSignedIn: null,
+    userId: null
 };
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'SIGN_IN':
+        case SIGN_IN:
             // ...spread method: takes the original value ([array] or {object}) and turns it into set of items
             // These items can than be modifies such as below, where isSignedIn value is updated
-            return { ...state, isSignedIn: true };
-        case 'SIGN_OUT':
-            return { ...state, isSignedIn: false};
+            return { ...state, isSignedIn: true, userId: action.payload };
+        case SIGN_OUT:
+            return { ...state, isSignedIn: false, userId: null};
         default:
             return state;
     }
