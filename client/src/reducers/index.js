9d6abb4a66da24c2b81e40cf04c9fe0643d5c8a6
ThomasMartinUK <tmuk@protonmail.com>
2019-06-22 10:34:16 +0100

    Added reducers to Delete stream and to Fetch all streams. Using lodash library for a more clean code. Wired up the streamReducer into the Redux store of the App

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import authReducer from './authReducer';
+import streamReducer from './streamReducer';
 
 export default combineReducers({
     auth: authReducer,
     // Redux form must be declared with key form: <name>
-    form: formReducer
+    form: formReducer,
+    streams: streamReducer
 });
--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 import {
     FETCH_STREAM,
     FETCH_STREAMS,
@@ -8,12 +10,19 @@ import {
 
 export default (state = {}, action) => {
     switch (action.type) {
+        case FETCH_STREAMS:
+            // Lodash mapKeys method; takes an array of objects[{}], and assigns a specific value as a new key and assigns the original object{} to that key
+            // In this case: we take the array of object returned inside action.payload, use the 'id' property as a new key and assign the original objects{} to the new key
+            // Reason for ...spread on mapKeys; a large object{} is returned, we want to take the key:value pairs from that object and add them to the new object{} we have created
+            return { ...state, ..._.mapKeys(action.payload, 'id') };
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case CREATE_STREAM:
             return { ...state, [action.payload.id]: action.payload };
         case EDIT_STREAM:
             return { ...state, [action.payload.id]: action.payload };
+        case DELETE_STREAM:
+            return _.omit(state, action.payload);
         default:
             return state;
     }
