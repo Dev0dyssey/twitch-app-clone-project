7b6babb1a9c745bc86114c11d2e6c79967e0a2ea
ThomasMartinUK <tmuk@protonmail.com>
2019-06-24 20:27:49 +0100

    Created two helper methods to render Admin buttons and Create New Stream button whenever a user is logged in and if the logged in userId mathces the stream userId inside the App

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchStreams } from '../../actions';
 
 class StreamList extends React.Component {
@@ -7,10 +8,28 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    // Helper function to render the Delete & Edit buttons
+    renderAdmin(stream) {
+        if(stream.userId === this.props.currentUserId) {
+            return (
+                <div className = "right floated content">
+                    <button className = "ui button primary">
+                        Edit
+                    </button>
+                    <button className = "ui button negative">
+                        Delete
+                    </button>
+                </div>
+            );
+        }
+    }
+
     renderList() {
         return this.props.streams.map(stream => {
             return(
                 <div className="item" key = {stream.id}>
+                    {/* Reason for the button to be called at the beginning of the code; Semantic UI rules - to style the element properly the code should be placed at the top of the element code */}
+                    {this.renderAdmin(stream)}
                     <i className = "large middle aligned icon camera" />
                     <div className = "content">
                         {stream.title}
@@ -23,6 +42,19 @@ class StreamList extends React.Component {
         })
     }
 
+    // Helper method to show a Create Stream button when a user is signed in
+    renderCreate() {
+        if(this.props.isSignedIn) {
+            return(
+                <div style={{ textAlign: 'right' }}>
+                    <Link to="/streams/new" className="ui button primary">
+                        Create Stream
+                    </Link>
+                </div>
+            );
+        }
+    }
+
     render() {
         return (
             <div>
@@ -30,15 +62,18 @@ class StreamList extends React.Component {
                 <div className = "ui celled list">
                     {this.renderList()}
                 </div>
+                {this.renderCreate()}
             </div>
-        )
+        );
     }
 }
 
 const mapStateToProps = state => {
     return {
         // Object.<name> ; built in JavaScript function(takes object{} as an argument). Pulls all value of an object{}, and inserted into an array
-        streams: Object.values(state.streams)
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
     }
 }
 
