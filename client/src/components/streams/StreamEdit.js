0266ca25154fd53624f1caa651c1de51488c6ba4
ThomasMartinUK <tmuk@protonmail.com>
2019-06-27 07:03:41 +0100

    Added link to the <StreamEdit /> component; using url navigation to navigate to a stream with a specific ID. Using the ownProps and state arguments to pull data odd the Streams databse object to retrieve individual stream information. This information will be shown to the user to edit inside the App

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamEdit = () => {
+
+const StreamEdit = (props) => {
+    // Reason we can use props; the StreamEdit component is being rendered via a Route from React-Router-DOM. This adds a list of props onto the render which we can than use
+    console.log(props);
     return <div>StreamEdit</div>
 };
 
-export default StreamEdit;
+// ownProps; second argument that can be passed into the mapStateToProps method. References the props passed into the component from outside of the Redux state store
+// Such as the props being passed by the Route from React-Router-DOM
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] };
+}
+
+export default connect(mapStateToProps)(StreamEdit);
