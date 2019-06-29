e1fe603bd306d01cc1f687e26d9eb0df0de0bc97
ThomasMartinUK <tmuk@protonmail.com>
2019-06-29 16:28:06 +0100

    Wired up the <StreamForm /> component into the <StreamEdit /> component, allowing for edits to be made to the name and description. Using InitalValues property of Redux Form to pass the values from the stream object and display that information inside of the App

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,18 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends React.Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
     }
 
+    onSubmit = (formValues) => {
+        console.log(formValues);
+    };
+
     render(){
         // Reason we can use props; the StreamEdit component is being rendered via a Route from React-Router-DOM. This adds a list of props onto the render which we can than use
         if(!this.props.stream) {
             return <div>Loading...</div>
         }
-        return <div>{this.props.stream.title}</div>
+        return(
+            <div>
+                <h3>Edit a Stream</h3>
+                {/* InitialValues; very specific property on Redux-Form. Reason we can use it is because the form itself is wrapped inside of ReduxForm({}) */}
+                {/* See <StreamForm /> for the wrapping example */}
+                {/* InitialValues names MUST match the input field names from the form itself. name === name NOT names !== name */}
+                <StreamForm 
+                    initialValues = { this.props.stream }
+                    onSubmit = {this.onSubmit}
+                />
+            </div>
+        );
     }
 }
 
@@ -22,4 +38,4 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit);
