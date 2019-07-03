f404c188799be7d06092a5aa0eee23befe28fcf1
ThomasMartinUK <tmuk@protonmail.com>
2019-07-03 20:38:42 +0100

    Added styling to the <Modal /> component, using programmatic navigation to allow user to click out of the Modal and have the Modal disappear. Utilised stopPropagation to ensure correct functionality of <Modal /> within the App

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import history from '../history';
 
 // Portal creation and definition
 const Modal = props => {
     // The initial argument of createPortal; whatever we want to show to the user
     return ReactDOM.createPortal(
-        <div className = "ui dimmer modals visible active">
-            <div className = "ui standard modal visible active">
-                Modal Testing Underway
+        // Using programmatic navigation we can direct the user to another page, allowing the modal to "disappear" when user click outside of its area
+        <div onClick = {() => history.push('/')} className = "ui dimmer modals visible active">
+            {/* e.stopPropagation; stops the event propagation up the element. This ensures that only when the user clicks outside of the body, will the modal disappear */}
+            <div onClick = {(e) => e.stopPropagation()} className = "ui standard modal visible active">
+                <div className = "header">Delete Stream</div>
+                <div className = "content">Are you sure you want to delete the stream?</div>
+                <div className = "actions">
+                    <button className = "ui primary button">Delete</button>
+                    <button className = "ui button">Cancel</button>
+                </div>
             </div>
     {/* Second argument of createPortal; reference to the element we want to render the portal/modal into */}
     {/* Often done by creating a sibling html element inside if index.html file, allowing us to attach the portal to that element */}
