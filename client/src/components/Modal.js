7d162cbeed53f5cb98359a5605bcacc615effabc
ThomasMartinUK <tmuk@protonmail.com>
2019-07-04 06:30:34 +0100

    Moved Modal content into props being passed from the parent <StreamDelete /> to allow Modal reusability. Added React Fragments to <StreamDelete /> to help with action button layout within <Modal />. Added <Link /> to <StreamList /> to allow the user to navigate to the Delete Modal of the correct Stream (using the stream ID). Modal hidden when user clicks outside of its body, also handled via props being passed down inside the App

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
 // Portal creation and definition
 const Modal = props => {
     // The initial argument of createPortal; whatever we want to show to the user
     return ReactDOM.createPortal(
-        // Using programmatic navigation we can direct the user to another page, allowing the modal to "disappear" when user click outside of its area
-        <div onClick = {() => history.push('/')} className = "ui dimmer modals visible active">
+        <div onClick = {props.onDismiss} className = "ui dimmer modals visible active">
             {/* e.stopPropagation; stops the event propagation up the element. This ensures that only when the user clicks outside of the body, will the modal disappear */}
+            {/* Content of the Modal coming from the props when <Modal /> is rendered inside of <StreamDelete /> */}
             <div onClick = {(e) => e.stopPropagation()} className = "ui standard modal visible active">
-                <div className = "header">Delete Stream</div>
-                <div className = "content">Are you sure you want to delete the stream?</div>
+                <div className = "header">{props.title}</div>
+                <div className = "content">{props.conent}</div>
                 <div className = "actions">
-                    <button className = "ui primary button">Delete</button>
-                    <button className = "ui button">Cancel</button>
+                    {props.actions}
                 </div>
             </div>
     {/* Second argument of createPortal; reference to the element we want to render the portal/modal into */}
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import Modal from '../Modal';
+import history from '../../history';
 
 const StreamDelete = () => {
+    // Helper method to assist with rendering action buttons as props on the <Modal /> component
+    const actions = (
+        // Use of React fragments <></> to assist with styling of the <Modal /> component, as Semantic UI does not render the buttons layout correctly when using <div></div>
+        // <></> Allows for the buttons to be evenly spaced even as the viewport size changes
+        // Alternative way of writing the fragment; <React.Fragment></React.Fragment> used on code quality checkers that do not recognize <></> as valid syntax
+        <React.Fragment>
+            <button className = "ui button negative">Delete</button>
+            <button className = "ui button">Cancel</button>
+        </React.Fragment>
+    );
+
     return (
         <div>
             StreamDelete
-            <Modal />
+            <Modal 
+                title = "Delete Stream"
+                conent = "Are you sure you want to delete this stream?"
+                actions = {actions}
+                // Using programmatic navigation we can direct the user to another page, allowing the modal to "disappear" when user click outside of its area
+                onDismiss = {() => history.push('/')}
+            />
         </div>
     );
 };
